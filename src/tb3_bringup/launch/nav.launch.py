7777b5710f44faffb8e112bb9ca724b50332d9dc
Sun Qin <sqlxx@163.com>
2025-09-12 18:19:52 +0800

    support use_acml parameter

--- src/tb3_bringup/launch/nav.launch.py
+++ src/tb3_bringup/launch/nav.launch.py
@@ -11,25 +11,34 @@ def generate_launch_description():
   use_composition = LaunchConfiguration('use_composition')
   map_yaml_file = LaunchConfiguration('map')
   log_level = LaunchConfiguration('log_level')
+  use_amcl = LaunchConfiguration('use_amcl')
+  container_name = 'tb3_container'
 
   declare_map_yaml_cmd = DeclareLaunchArgument('map', description='Full path to map yaml file to load')
-  declare_use_sim_time_cmd = DeclareLaunchArgument('use_sim_time', default_value='true')
-  declare_use_composition_cmd = DeclareLaunchArgument('use_composition', default_value='true')
+  declare_use_sim_time_cmd = DeclareLaunchArgument('use_sim_time', default_value='True')
+  declare_use_composition_cmd = DeclareLaunchArgument('use_composition', default_value='False')
   declare_log_level_cmd = DeclareLaunchArgument('log_level', default_value='info')
+  declare_use_amcl_cmd = DeclareLaunchArgument('use_amcl', description='use amcl if True, use slam_toolbox if False', default_value='False')
 
   bringup_node_group = GroupAction([
     Node(
       condition=IfCondition(use_composition),
-      name="tb3_container",
+      name=container_name,
       package='rclcpp_components',
       executable='component_container_isolated',
-      parameters=[{'autostart': True}],
+      parameters=[{'autostart': 'true'}],
       arguments=['--ros-args', '--log-level', log_level],
       output='screen'
     ),
     IncludeLaunchDescription(
       PathJoinSubstitution([FindPackageShare('tb3_navigation'), 'launch', 'tour_nav.launch.py']),
-      launch_arguments={'use_sim_time': use_sim_time, 'map': map_yaml_file, 'log_level': log_level}.items()
+      launch_arguments={
+        'use_sim_time': use_sim_time, 
+        'map': map_yaml_file, 
+        'log_level': log_level, 
+        'use_composition': use_composition, 
+        'container_name': container_name,
+        'use_amcl': use_amcl}.items()
     ),
     IncludeLaunchDescription(
       PathJoinSubstitution([FindPackageShare('tb3_rosbridge'), 'launch', 'rosbridge.launch.py'])
@@ -42,6 +51,7 @@ def generate_launch_description():
   ld.add_action(declare_use_sim_time_cmd)
   ld.add_action(declare_use_composition_cmd)
   ld.add_action(declare_log_level_cmd)
+  ld.add_action(declare_use_amcl_cmd)
 
   ld.add_action(bringup_node_group)
 
--- src/tb3_navigation/launch/tour_nav.launch.py
+++ src/tb3_navigation/launch/tour_nav.launch.py
@@ -2,33 +2,18 @@ import os
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from nav2_common.launch import RewrittenYaml
-from launch_ros.descriptions import ParameterFile
+from launch.conditions import IfCondition
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
   use_sim_time = LaunchConfiguration('use_sim_time')
   map_yaml_file = LaunchConfiguration('map')
-  # params_file = LaunchConfiguration("params_file", default=os.path.join(get_package_share_directory('tb3_navigation'), 'config', 'config.yaml'))
   log_level = LaunchConfiguration('log_level')
-
-  param_substitutions = {
-    'use_sim_time': use_sim_time,
-    'yaml_filename': map_yaml_file
-  }
-
-  configured_params = ParameterFile(
-    RewrittenYaml(
-      source_file=os.path.join(get_package_share_directory('tb3_navigation'), 'config', 'config.yaml'),
-      param_rewrites=param_substitutions,
-      convert_types=True
-    ), allow_substs=True
-  )
-  
-  slam_lifecycle_nodes = ['map_saver']
-  localization_lifecycle_nodes = ['map_server']
+  use_amcl = LaunchConfiguration('use_amcl')
+  use_slam = LaunchConfiguration('use_slam')
+  use_composition = LaunchConfiguration('use_composition')
+  container_name = LaunchConfiguration('container_name')
 
   return LaunchDescription([
     DeclareLaunchArgument(
@@ -41,46 +26,42 @@ def generate_launch_description():
     ),
     DeclareLaunchArgument(
       'log_level',
+      description='log level',
       default_value='info'
     ),
-    Node(
-      package='nav2_lifecycle_manager',
-      executable='lifecycle_manager',
-      name='lifecycle_manager_slam',
-      output='screen',
-      arguments=['--ros-args', '--log-level', log_level],
-      parameters=[{'use_sim_time': use_sim_time}, {'node_names': slam_lifecycle_nodes}, {'autostart': True}]
+    DeclareLaunchArgument(
+      'use_amcl',
+      description='Use amcl if true, use slam toolbox if false',
+      default_value='False'
     ),
-    Node(
-      package='nav2_map_server',
-      executable='map_saver_server',
-      output='screen',
-      respawn=False,
-      respawn_delay=2.0,
-      arguments=['--ros-args', '--log-level', log_level],
-      parameters=[configured_params]
+    DeclareLaunchArgument(
+      'use_slam',
+      description='Use SLAM if true',
+      default_value='False'
     ),
-    Node(
-      package='nav2_lifecycle_manager',
-      executable='lifecycle_manager',
-      name='lifecycle_manager_localization',
-      output='screen',
-      arguments=['--ros-args', '--log-level', log_level],
-      parameters=[{'use_sim_time': use_sim_time}, {'node_names': localization_lifecycle_nodes}, {'autostart': True}]
+    DeclareLaunchArgument(
+      'use_composition',
+      description='Use container for nav nodes if true',
+      default_value='False'
     ),
-    Node(
-      package='nav2_map_server',
-      executable='map_server',
-      name='map_server',
-      output='screen',
-      parameters=[configured_params],
-      arguments=['--ros-args', '--log-level', log_level]
+    DeclareLaunchArgument(
+      'container_name',
+      description='the name of the container that nodes will load in to if use_composition is true',
+      default_value='nav_container'
     ),
     IncludeLaunchDescription(
-      PathJoinSubstitution([FindPackageShare('tb3_slam'), 'launch', 'slam_toolbox_localization.launch.py']),
-      launch_arguments={'use_sim_time': use_sim_time, 'log_level': log_level}.items()
+      PathJoinSubstitution([FindPackageShare('tb3_navigation'), 'launch', 'slam.launch.py']),
+      launch_arguments={'use_sim_time': use_sim_time, 'log_level': log_level}.items(),
+      condition=IfCondition(use_slam)
+    ),
+    IncludeLaunchDescription(
+      PathJoinSubstitution([FindPackageShare('tb3_navigation'), 'launch', 'localization.launch.py']),
+      launch_arguments={
+        'use_sim_time': use_sim_time, 
+        'log_level': log_level, 
+        'map': map_yaml_file, 
+        'use_composition': use_composition, 
+        'container_name': container_name,
+        'use_amcl': use_amcl}.items(),
     )
   ])
-
-  
-
